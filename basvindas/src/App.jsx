a599d1024648867d4368d8f3bafa9d3c38621a36
cavzxnn <carolcosta2009@icloud.com>
2025-04-08 12:22:55 -0300

    8/04

---
+++ basvindas/src/App.jsx
@@ -0,0 +1,22 @@
+import BasVindas from "./exemplo1"
+function App() {
+ 
+return (
+    <div>
+      <h1> Boas Vindas ao React com JavaScript</h1>
+      <p>
+        Esse é o arquivo App.jsx que será o nosso ponto de partida para o desenvolvimnto de aplicação React utilizado JavaScript.
+
+      </p>
+      <BasVindas nome="Carol"/>
+      <BasVindas nome="Mateus"/>
+      <BasVindas nome="Jair"/>
+      <BasVindas nome="Ana Jardim"/>
+      <BasVindas nome="isa"/>
+      <BasVindas nome="nanaty"/>
+      <BasVindas nome="eduardo"/>
+      </div>
+  )
+}
+
+export default App
---
+++ basvindas/src/exemplo1.jsx
@@ -0,0 +1,13 @@
+export default function BasVindas ({nome})
+{
+   
+return (
+    <div>
+        <p>
+            Olá {nome} seja bem vindo ao React.
+        </p>
+    </div>
+)
+}
+
+
